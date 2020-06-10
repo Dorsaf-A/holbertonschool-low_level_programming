fc3fa0184c5fb3e923c839d5d761917a87b96020
Dorsaf-A <63168823+Dorsaf-A@users.noreply.github.com>
2020-06-10 23:32:50 +0200

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,17 @@
 #include<stdio.h>
 /**
- *main - size
- *Return: 0
- */
-int main(void) {
+*main - size
+*Return: 0
+*/
+int main(void) 
+{
 int intType;
 float floatType;
 double doubleType;
 char charType;
-printf("Size of int: %ld bytes\n", sizeof(intType));
-printf("Size of float: %ld bytes\n", sizeof(floatType));
-printf("Size of double: %ld bytes\n", sizeof(doubleType));
-printf("Size of char: %ld byte\n", sizeof(charType));
+printf("Size of int: %ld bytes", sizeof(intType));
+printf("Size of float: %ld bytes", sizeof(floatType));
+printf("Size of double: %ld bytes", sizeof(doubleType));
+printf("Size of char: %ld byte", sizeof(charType));
 return (0);
 }
