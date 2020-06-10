088a50f90dbb97801269b1266be8944d3ea059dc
Dorsaf-A <63168823+Dorsaf-A@users.noreply.github.com>
2020-06-10 23:54:59 +0200

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,9 +5,9 @@
 */
 int main(void) 
 {
-printf("Size of int : %d\n",sizeof(int));
-printf("Size of char : %d\n",sizeof(char));
-printf("Size of float data : %d\n",sizeof(float));
-printf("Size of double : %d\n",sizeof(double));    
+printf("Size of int: %d\n",sizeof(int));
+printf("Size of char: %d\n",sizeof(char));
+printf("Size of float data: %d\n",sizeof(float));
+printf("Size of double: %d\n",sizeof(double));    
 return (0);
 }
