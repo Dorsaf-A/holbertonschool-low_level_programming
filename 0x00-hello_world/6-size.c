dcd05435d905dabd660eadb009f29239b51d7414
Dorsaf-A <63168823+Dorsaf-A@users.noreply.github.com>
2020-06-10 23:08:11 +0200

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,5 +12,5 @@ printf("Size of int: %ld bytes\n", sizeof(intType));
 printf("Size of float: %ld bytes\n", sizeof(floatType));
 printf("Size of double: %ld bytes\n", sizeof(doubleType));
 printf("Size of char: %ld byte\n", sizeof(charType));
-return 0;
+return (0);
 }
