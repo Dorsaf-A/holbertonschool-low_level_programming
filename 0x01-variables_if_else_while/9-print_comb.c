7f8de0a4b119b9e0aad440031116309db2dd7e12
Dorsaf-A <63168823+Dorsaf-A@users.noreply.github.com>
2020-06-11 23:45:27 +0200

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,12 +10,15 @@ int main(void)
 {
 int n;
 
-for (n = '0'; n <= '8'; n++)
+for (n = '0'; n <= '9'; n++)
 {
-putchar(n);
-putchar(', ');
+if (n != '0' && n != '9')
+{
+putchar(',');
+putchar(' ');
 }
-putchar('9');
+putchar(n);
+}
 putchar('\n');
 return (0);
 }
