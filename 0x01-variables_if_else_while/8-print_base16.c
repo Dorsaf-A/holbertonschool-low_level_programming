b52fc2b16e97b528e16d2ab7809ecb800a39cd66
Dorsaf-A <dorsaf.aribi@yahoo.com>
2020-06-11 14:36:08 +0000

    base 16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,9 +9,9 @@
 int main(void)
 {
 int ch;
-for (ch = '0'; ch >= '9'; ch++)
+for (ch = '0'; ch <= '9'; ch++)
 putchar(ch);
-for (ch = 'a'; ch >= 'f'; ch++)
+for (ch = 'a'; ch <= 'f'; ch++)
 putchar(ch);
 putchar('\n');
 return (0);
