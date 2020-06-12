988338be7e025e124d3deb0b485af278eaf2ec70
Dorsaf-A <dorsaf.aribi@yahoo.com>
2020-06-12 09:19:17 +0000

    c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+	 #include <stdio.h>
+	 #include <stdlib.h>
+	 #include <time.h>
+	 /**
+	  * main - Entry point
+	  *
+	  * Return: Always 0 (Success)
+	  */
+	 int main(void)
+{
+	int i, j, c;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = i+1; j <= '9'; j++)
+		{
+			for (c = j + 1; c <= '9'; c++)
+			{
+				putchar(i);
+				putchar(j);
+				putchar(c);
+				if (i == '7' && j == '8' && c =='9')
+				{
+				}
+				else
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
