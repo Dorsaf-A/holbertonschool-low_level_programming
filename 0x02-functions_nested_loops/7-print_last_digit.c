339aa14d7ea95357ae7fe4dd56ce80ff7df97ae1
Dorsaf-A <dorsaf.aribi@yahoo.com>
2020-06-16 13:53:56 +0000

    return 4

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * print_last_digit - check the code for Holberton School students.
+ * @n: int
+ * Return: 1 or 0 or -1
+ */
+int print_last_digit(int n)
+{
+	n = _abs(n);
+	_putchar((n % 10) + '0');
+	return (4);
+}
