4cdbd28a325dc7be071e00b156660de7c50ac993
Dorsaf-A <dorsaf.aribi@yahoo.com>
2020-06-16 14:01:37 +0000

    return 4

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * _abs - check the code for Holberton School students.
+ * @n: int
+ * Return: 1 or 0 or -1
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	return (-n);
+}
