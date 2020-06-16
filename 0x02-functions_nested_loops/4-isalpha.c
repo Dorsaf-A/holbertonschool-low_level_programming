83a71836a674077fff843db129862a4b4dc50e54
Dorsaf-A <63168823+Dorsaf-A@users.noreply.github.com>
2020-06-16 11:30:00 +0200

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,13 +1,13 @@
 #include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
- *
+ * _isalpha - check the code for Holberton School students.
+ * @c: int
  * Return: Always 0.
  */
 int _isalpha(int c)
 {
-	if((c <= 'z' && c >= 'a') || (c <= 'Z' && c >= 'A')
+	if ((c <= 'z' && c >= 'a') || (c <= 'Z' && c >= 'A')
 	   return (1);
 	   return (0);
 }
