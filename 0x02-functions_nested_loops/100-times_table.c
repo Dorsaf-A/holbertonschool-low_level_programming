8bd79c3f7337ef8e6531583e602cd20deb35f515
Dorsaf-A <63168823+Dorsaf-A@users.noreply.github.com>
2020-06-18 01:58:05 +0200

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,1 +1,50 @@
 #include "holberton.h"
+
+/**
+ * times_table - multiplication table
+ *
+ *
+ */
+void print_times_table(int n)
+{
+int a, i, j;
+
+for (i = 0; i <= n; i++)
+{
+	for (j = 0; j <= n; j++)
+	{
+
+		a = i * j;
+		if (j != 0)
+		{
+		_putchar(',');
+		_putchar(' ');
+		}
+		if (a > 99)
+    {
+			_putchar(a / 100 + 48);
+			_putchar((a / 10) % 10 + 48);
+      _putchar(a % 10 + 48);
+		}
+    else if (a > 9)
+    {
+      _putchar(' ');
+      _putchar(a / 10 + 48);
+      _putchar(a % 10 + 48);
+    }
+		else if (j == 0)
+		{
+			_putchar(a + 48);
+		}
+		else
+		{
+      _putchar(' ');
+			_putchar(' ');
+			_putchar(a + 48);
+		}
+
+	}
+
+	_putchar('\n');
+}
+}
