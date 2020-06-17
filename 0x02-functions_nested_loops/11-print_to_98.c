a6c461882968533670192888b115e37988657695
Dorsaf-A <dorsaf.aribi@yahoo.com>
2020-06-17 23:41:57 +0000

    print all

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,8 +8,10 @@
  */
 void print_to_98(int n)
 {
-	for ( ; n <= 98; n++)
-		printf("%d", n);
-	for ( ; n >= 98; n--)
-		printf("%d", n);
+	for ( ; n < 98; n++)
+		printf("%d, ", n);
+
+	for ( ; n > 98; n--)
+		printf("%d, ", n);
+	printf("%d\n",98);
 }
