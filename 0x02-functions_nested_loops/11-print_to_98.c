a15673fd95c75a0b0301ddc1f388a01ac6f6369f
Dorsaf-A <dorsaf.aribi@yahoo.com>
2020-06-17 23:35:38 +0000

    print all

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * print_to_98 - check the code for Holberton School students.
+ * @n: integer
+ *
+ */
+void print_to_98(int n)
+{
+	for ( ; n <= 98; n++)
+		printf("%d", n);
+	for ( ; n >= 98; n--)
+		printf("%d", n);
+}
