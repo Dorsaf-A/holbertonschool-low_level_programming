9e4b41a51cfae7b773a43abdb75680fc398293da
Dorsaf-A <dorsaf.aribi@yahoo.com>
2020-06-17 22:53:08 +0000

    nested loop

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,9 +1,8 @@
 #include "holberton.h"
 
 /**
- * print_last_digit - check the code for Holberton School students.
- * @n: int
- * Return: 1 or 0 or -1
+ * jack_bauer - check the code for Holberton School students.
+ *
  */
 void jack_bauer(void)
 {
