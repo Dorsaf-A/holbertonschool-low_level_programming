07b67750e3bdd2c5e763b2f152689a2e8d1d30c8
Dorsaf-A <63168823+Dorsaf-A@users.noreply.github.com>
2020-06-18 01:04:24 +0200

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,21 +9,25 @@ void times_table(void)
 {
 int a, i, j;
 
-for (i= 0; i <= 9; i++)
+for (i = 0; i <= 9; i++)
 {
 	for (j = 0; j <= 9; j++)
 	{
 
 		a = i * j;
-		if(j != 0)
+		if (j != 0)
 		{
 		_putchar(',');
-		 _putchar(' ');
+		_putchar(' ');
 		}
 		if (a > 9)
 		{
 			_putchar(a / 10 + 48);
 			_putchar(a % 10 + 48);
+		}
+		else if (j == 0)
+		{
+			_putchar(a + 48);
 		}
 		else
 		{
