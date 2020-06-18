9bc3141c02d335479dd6ae42303c6807ac917cb9
Dorsaf-A <63168823+Dorsaf-A@users.noreply.github.com>
2020-06-19 00:48:42 +0200

    Update 100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,13 +1,25 @@
 #include <stdio.h>
 
 /**
-*
-*
+* main - prime factor
+* @n: integer
 *
 *
 */
 int main(int n)
 {
+int i, j, r;
 
+i = n;
+while (i <= n && i > 2)
+{
+if (n % i == 0)
+for (j = 1; j < i; i++)
+{
+if (i % j == 0)
+print("%d\n", i);
+}
+i--;
+}
 	return(0);
 }
