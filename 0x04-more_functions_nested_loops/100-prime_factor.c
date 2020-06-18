28c3449ce083d6b44d23a5910653ad4496b3a11a
Dorsaf-A <dorsaf.aribi@yahoo.com>
2020-06-18 22:42:27 +0000

    advanced

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+*
+*
+*
+*
+*/
+int main(int n)
+{
+
+	return(0);
+}
---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+/**
+ * print_number - print number
+ * @n:integer
+ *
+ */
+void print_number(int n)
+{
+
+}
