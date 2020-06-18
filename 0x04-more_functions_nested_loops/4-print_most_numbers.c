10c35fea1b62f81c4dcedb643f02a743ac9d87bc
Dorsaf-A <63168823+Dorsaf-A@users.noreply.github.com>
2020-06-18 15:35:35 +0200

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -12,15 +12,10 @@ void print_most_numbers(void)
 int a;
 
 a = 0;
-while (a <= 14)
+while (a <= 9)
 {
 	if (a != 2 && a != 4)
-	{
-		if (a > 9)
-			_putchar(a / 10 + '0');
 		_putchar(a % 10 + '0');
-		
-  	}
 	a++;
 }
 _putchar('\n');
