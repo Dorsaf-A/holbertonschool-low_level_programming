06df2c3e041137d473829f6817f26c41681714da
Dorsaf-A <dorsaf.aribi@yahoo.com>
2020-06-18 14:03:40 +0000

    diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * print_diagonal - diagonal line
+ * @n: integer
+ *
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= a; i++)
+		{
+			for (j = 1; j < i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+	}
+	}
+}
