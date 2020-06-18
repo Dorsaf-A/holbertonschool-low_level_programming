2d460a73c73db1370a7320bdb71510456e5ec804
Dorsaf-A <dorsaf.aribi@yahoo.com>
2020-06-18 21:59:38 +0000

    success

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * print_triangle - print triangle with #
+ * @size: integer
+ *
+ */
+
+void print_triangle(int size)
+{
+	int i, j, c;
+
+	for (i = 1; i <= size; i++)
+	{
+		for (j = size - i; j < size && j > 0; j--)
+			_putchar(' ');
+		for (c = size - i; c < size; c++)
+			_putchar('#');
+		_putchar('\n');
+	}
+}
