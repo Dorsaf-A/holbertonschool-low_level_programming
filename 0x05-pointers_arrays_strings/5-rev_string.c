1bfc9db30ae0dae534aae682c61c1f44d97e4b35
Dorsaf-A <63168823+Dorsaf-A@users.noreply.github.com>
2020-06-24 00:26:06 +0200

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,4 +1,20 @@
+/**
+* rev_string - a function that reverse a string
+* @s: char pointer
+*
+*/
+
 void rev_string(char *s)
 {
+int i, j;
+string c;
 
+for (i = 0; s[i] != '\0'; i++)
+{
+  c[i] = s[i];
+}
+for (j = 0; j <= i; j++)
+{
+  s[j] = c[i - j];	
+}
 }
