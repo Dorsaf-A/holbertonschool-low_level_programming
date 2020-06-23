2d34a12d5da0ca61a79e4bf431e33935fcc7d93a
Dorsaf-A <63168823+Dorsaf-A@users.noreply.github.com>
2020-06-24 00:08:58 +0200

    pointer

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,13 +7,12 @@
 void print_rev(char *s)
 {
 	int i;
-  for (i = 0; str[i] != '\0'; i++)
-  {
-  }
-	for (i; str[i] != '\0'; i--)
-	{
-		_putchar(str[i]);
-	}
-	_putchar('\n');
+for (i = 0; str[i] != '\0'; i++)
+{
 }
+for (i; str[i] != '\0'; i--)
+{
+	_putchar(str[i]);
+}
+_putchar('\n');
 }
