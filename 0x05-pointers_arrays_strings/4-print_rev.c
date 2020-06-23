f0ef6469aebdbc7900e23b4bf0834ae4bc6d4a90
Dorsaf-A <63168823+Dorsaf-A@users.noreply.github.com>
2020-06-24 00:06:56 +0200

    pointer

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,4 +1,19 @@
+#include "holberton.h"
+/**
+ * print_rev - puts reverse
+ * @s: char pointer
+ */
+
 void print_rev(char *s)
 {
-
+	int i;
+  for (i = 0; str[i] != '\0'; i++)
+  {
+  }
+	for (i; str[i] != '\0'; i--)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
 }
