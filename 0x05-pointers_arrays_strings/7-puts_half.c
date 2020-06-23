c2620ba09de457e28205417bfec728e7967c91d2
Dorsaf-A <63168823+Dorsaf-A@users.noreply.github.com>
2020-06-24 00:59:15 +0200

    update

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,20 +6,20 @@
 
 void puts_half(char *str)
 {
-	int i, j;
+int i, j;
 
-	for (i = 0; str[i] != '\0'; i++)
-	{
-	}
-  if (i % 2 == 0)
-    for (j = i / 2; j < i; j++)
-	  {
-        _putchar(str[j]);
-	  }
-  else
-    for (j = (i - 1) / 2; j < i; j++)
-	  {
-        _putchar(str[j]);
-	  }
-	_putchar('\n');
+for (i = 0; str[i] != '\0'; i++)
+{
+}
+if (i % 2 == 0)
+for (j = i / 2; j < i; j++)
+{
+_putchar(str[j]);
+}
+else
+for (j = (i - 1) / 2; j < i; j++)
+{
+_putchar(str[j]);
+}
+_putchar('\n');
 }
