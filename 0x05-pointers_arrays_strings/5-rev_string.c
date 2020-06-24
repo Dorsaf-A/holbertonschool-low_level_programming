f5049e5fd6411d60a297ac8e86b5871405788171
Dorsaf-A <63168823+Dorsaf-A@users.noreply.github.com>
2020-06-24 14:37:28 +0200

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,13 +7,14 @@
 void rev_string(char *s)
 {
 int i, j;
-char *c = s;
+char c[200];
 
 for (i = 0; s[i] != '\0'; i++)
 {
+    c[i] = s[i];
 }
-for (j = 0; j < i; j++)
+for (j = 0; j != i; j++)
 {
-s[j] = c[i + 1 - j];
+  s[j] = c[i - 1 -j];	
 }
 }
