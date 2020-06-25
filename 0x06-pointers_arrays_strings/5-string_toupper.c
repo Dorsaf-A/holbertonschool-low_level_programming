194efdd09de53544fafe5b487f143b740ab4fec5
Dorsaf-A <dorsaf.aribi@yahoo.com>
2020-06-25 22:41:32 +0000

    update

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,15 @@
+/**
+ *string_toupper - string toupper
+ *@c: char pointer
+ *Return: char
+ *
+ */
+char *string_toupper(char *c)
+{
+	int i;
+
+	for (i = 0; c[i] != '\0'; i++)
+		if (c[i] >= 97 && c[i] <= 122)
+			c[i] -= 32;
+	return (c):
+}
