6e1c84fc17c39d4093af30ad1d74c611a0f5495c
Dorsaf-A <dorsaf.aribi@yahoo.com>
2020-06-25 19:46:36 +0000

    update

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+/**
+ * _strcmp - compare two strings
+ * @s1: char pointer
+ * @s2: char pointer
+ *
+ * Return: difference
+ *
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int n;
+
+	n = s1 - s2;
+	return (n);
+}
