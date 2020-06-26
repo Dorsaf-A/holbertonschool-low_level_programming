8d4548598165516ccec7035e9f667c6370f9e0c1
Dorsaf-A <dorsaf.aribi@yahoo.com>
2020-06-26 22:43:21 +0000

    capitalize

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,18 @@
+/**
+ * cap_string - capitalizes all words
+ * @c: char pointer
+ * Return: string
+ *
+ *
+ */
+char *cap_string(char *c)
+{
+int i;
+
+for (i = 0; c[i] != '\0'; i++)
+{
+if ((c[i] == ' ' || c[i] == '\n' || c[i] == '\t') && c[i + 1] >= 97 && c[i + 1] <= 122)
+		c[i + 1] -= 32;
+}
+return (c);
+}
