afa8f4f6c51c83ba03c9ffd5719b734f57e0a713
Dorsaf-A <dorsaf.aribi@yahoo.com>
2020-06-29 22:49:55 +0000

    pointer

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,15 @@
+/**
+ * _memset - fills memory with a constant byte.
+ * @s: char pointer
+ * @b: char
+ * @n: unsigned integer
+ * Return: char
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+	return s;
+}
