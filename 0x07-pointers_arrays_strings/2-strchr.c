5cc0208b21ed32cf55621e34fa25c2943bf4a2c7
Dorsaf-A <dorsaf.aribi@yahoo.com>
2020-06-29 23:18:23 +0000

    pointer

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,15 @@
+/**
+ * _strchr -  locates a character in a string.
+ * @s: char pointer
+ * @c: char
+ * Return: char
+ *
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+		if (*s = c)
+			return (s);
+		s++;
+		return (NULL);
+}
